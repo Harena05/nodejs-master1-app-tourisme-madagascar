5beaffba2ca70174eb62f2befc97989e4c3bc866
05571 <fanilo.dev@et.in>
2023-08-03 11:18:41 +0300

    rename application start file

--- app.js
+++ app.js
@@ -1,72 +1,112 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const userRoutes = require('./routes/user.routes');
+const roleModel = require("./models/role.model");
+const authRoutes = require('./routes/auth.routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: "123456",
+    keys: ["COOKIE_SECRET"], // should use as secret environment variable
+    httpOnly: true
+  })
+);
 
 // Charger les variables d'environnement
 require('dotenv').config();
 
 // Récupérer les informations de connexion en fonction de l'environnement
-const dbHost = process.env.STATUS === 'prod'
-  ? process.env.DB_HOST_PROD
-  : process.env.DB_HOST_DEV;
-const dbName = process.env.STATUS === 'prod'
-  ? process.env.DB_NAME_PROD
-  : process.env.DB_NAME_DEV;
-const dbUser = process.env.STATUS === 'prod'
-  ? process.env.DB_USER_PROD
-  : process.env.DB_USER_DEV;
-const dbPass = process.env.STATUS === 'prod'
-  ? process.env.DB_PASS_PROD
-  : process.env.DB_PASS_DEV;
+const dbURI = process.env.STATUS === 'prod'
+  ? process.env.DB_CONNECTION_PROD
+  : process.env.DB_CONNECTION_DEV;
 
+  // const db = require("./app/models/db");
 // Connexion à MongoDB
-const dbURI = `mongodb://${dbUser}:${dbPass}@${dbHost}/${dbName}`;
+// const dbURI = `mongodb://${dbUser}:${dbPass}@${dbHost}/${dbName}`;
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => console.log('MongoDB connected'))
+  .then(() => {
+    console.log('MongoDB connected');
+    initial();
+  })
   .catch(err => console.error('MongoDB connection error:', err));
 
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-const userRoutes = require('./routes/user.rootes');
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Le reste de votre configuration Express et de vos routes...
 
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 app.use('/user', userRoutes);
+app.use('/auth',authRoutes);
+// require('./app/routes/auth.routes')(app);
+// require('./app/routes/user.routes')(app);
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+function initial() {
+  roleModel.estimatedDocumentCount().then((count)=>{
+    if(count ===0){
+      new roleModel({
+        rolename: "user"
+      }).save().then(()=>{
+        console.log("added 'user' to roles collection");
+      }).catch((err)=>{
+        console.log("error", err);
+      });
+
+      new roleModel({
+        rolename: "moderator"
+      }).save().then(()=>{
+        console.log("added 'moderator' to roles collection");
+      }).catch((err)=>{
+        console.log("error", err);
+      });
+
+      new roleModel({
+        rolename: "admin"
+      }).save().then(()=>{
+        console.log("added 'admin' to roles collection");
+      }).catch((err)=>{
+        console.log("error", err);
+      });
+    }
+  })
+  .catch((err)=>{
+    new roleModel({
+      rolename: "user"
+    }).save().then(()=>{
+      console.log("added 'user' to roles collection");
+    }).catch((err)=>{
+      console.log("error", err);
+    });
+
+    new roleModel({
+      rolename: "moderator"
+    }).save().then(()=>{
+      console.log("added 'moderator' to roles collection");
+    }).catch((err)=>{
+      console.log("error", err);
+    });
+
+    new roleModel({
+      rolename: "admin"
+    }).save().then(()=>{
+      console.log("added 'admin' to roles collection");
+    }).catch((err)=>{
+      console.log("error", err);
+    });
+  });
+}
+
+// Démarrer le serveur
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${port}`);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
